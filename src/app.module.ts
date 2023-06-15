3d73a37dfba95204db5ccbb265347702ad844bca
Juan <juanjo.gaviria23@hotmail.com>
2023-06-14 21:21:47 -0500

    Input, auth and tokens

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { RolesModule } from './roles/roles.module';
+import { AutenticacionModule } from './autenticacion/autenticacion.module';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'meigg',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsuariosModule,
+    RolesModule,
+    AutenticacionModule,
+  ],
+})
+export class AppModule {}
